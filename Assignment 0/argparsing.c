db23e1f5c7bb23e36064fa879a58a3a7786d923e
Martin Klampfer <e1526110@student.tuwien.ac.at>
2019-09-08 18:25:49 +0200

    added function to convert str to long

--- Assignment 0/argparsing.c
+++ Assignment 0/argparsing.c
@@ -3,18 +3,47 @@
 #include <string.h>
 #include <assert.h>
 #include <unistd.h>
+#include <errno.h>
+#include <limits.h>
 
 #define SIZE 10
 #define FILENAME "testfile"
 
+long convertToInt(char *arg) {
+    char *endptr;
+    long number;
+
+    //10 = decimal system, endptr is to check if strtol gave us a number
+    number = strtol(arg, &endptr, 10);
+
+	if ((errno == ERANGE && (number == LONG_MAX || number == LONG_MIN))
+			|| (errno != 0 && number == 0)) {
+        printf("Failed to convert input to number!\n");
+		exit(1);
+	}
+	if (endptr == arg) {
+        printf("No digits where found!\n");
+		exit(1);
+	}
+
+	/* If we got here, strtol() successfully parsed a number */
+	if (*endptr != '\0') { /* In principle not necessarily an error... */
+		printf("Further characters after number: %s\n", endptr);
+        exit(1);
+	}
+    return number;
+}
 
 int main(int argc, char *argv[]) {
-    char *a;
-    char *b;
+    char *progname;
+    long a;
+    long b;
 
-    //not yet done
+    if(argc > 0) {
+        progname = argv[0];
+    }
     if(argc != 3) {
-        printf("3 arguments required, usage: ./argparsing -aA -bB\n");
+        printf("Usage: %s -aA -bB\n", progname);
         exit(1);
     }
 
@@ -22,13 +51,13 @@ int main(int argc, char *argv[]) {
     while((opt = getopt(argc, argv, "a:b:")) != -1) {
         switch(opt) {
             case 'a':
-                a = optarg;
+                a = convertToInt(optarg);
                 break;
             case 'b':
-                b = optarg;
+                b = convertToInt(optarg);
                 break;
         }
     }
-    //TODO convert numbers to int
-    printf("A is %s and B is %s\n", a, b);
+
+    printf("A is %ld and B is %ld\n", a, b);
 }
