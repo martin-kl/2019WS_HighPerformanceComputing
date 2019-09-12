93f180632f1def6c7f2283ff88d63b12414d5fa3
Martin Klampfer <e1526110@student.tuwien.ac.at>
2019-09-12 19:57:59 +0200

    refactored time_diff method into extra file

--- Assignment_1/locality.c
+++ Assignment_1/locality.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <time.h>
+#include "time_diff.h"
 
 #define SIZE 1000
 
@@ -32,18 +33,6 @@ void col_sums2(double * sums, const double ** matrix, size_t nrs, size_t ncs) {
         }
     }
 }
-
-void timespec_diff(struct timespec *start, struct timespec *stop, struct timespec *result) {
-    if ((stop->tv_nsec - start->tv_nsec) < 0) {
-        result->tv_sec = stop->tv_sec - start->tv_sec - 1;
-        result->tv_nsec = stop->tv_nsec - start->tv_nsec + 1000000000;
-    } else {
-        result->tv_sec = stop->tv_sec - start->tv_sec;
-        result->tv_nsec = stop->tv_nsec - start->tv_nsec;
-    }
-    return;
-}
-
 
 
 // #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### 
---
+++ Assignment_1/time_diff.h
@@ -0,0 +1,6 @@
+#ifndef _H_TIMEDIFF
+#define _H_TIMEDIFF
+
+void timespec_diff(struct timespec *start, struct timespec *stop, struct timespec *result);
+
+#endif
