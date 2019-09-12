1533c38733e425181897489c52a2a6f3d47efaa9
Martin Klampfer <e1526110@student.tuwien.ac.at>
2019-09-12 19:51:40 +0200

    added reimplementation of col sum and refined output

--- Assignment_1/locality.c
+++ Assignment_1/locality.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <time.h>
 
 #define SIZE 1000
@@ -23,8 +24,16 @@ void col_sums(double * sums, const double ** matrix, size_t nrs, size_t ncs) {
   }
 }
 
-void timespec_diff(struct timespec *start, struct timespec *stop,
-                   struct timespec *result) {
+// Improved column sums implementation
+void col_sums2(double * sums, const double ** matrix, size_t nrs, size_t ncs) {
+    for (size_t i = 0; i < nrs; i++) {
+        for (size_t j = 0; j < ncs; j++) {
+            sums[j] += matrix[i][j];
+        }
+    }
+}
+
+void timespec_diff(struct timespec *start, struct timespec *stop, struct timespec *result) {
     if ((stop->tv_nsec - start->tv_nsec) < 0) {
         result->tv_sec = stop->tv_sec - start->tv_sec - 1;
         result->tv_nsec = stop->tv_nsec - start->tv_nsec + 1000000000;
@@ -32,7 +41,6 @@ void timespec_diff(struct timespec *start, struct timespec *stop,
         result->tv_sec = stop->tv_sec - start->tv_sec;
         result->tv_nsec = stop->tv_nsec - start->tv_nsec;
     }
-
     return;
 }
 
@@ -47,9 +55,11 @@ int main() {
     struct timespec diffcols;
     double * sumrows = (double*) malloc(sizeof(double) * SIZE);
     double * sumcols = (double*) malloc(sizeof(double) * SIZE);
+    //structs for improved col sums
+    struct timespec diffcols2;
+    double * sumcols2 = (double*) malloc(sizeof(double) * SIZE);
 
     // Avoiding memory fragmentation with row-major order:
-    int counter = 1;
     double * startaddr = (double*) malloc(sizeof(double) * SIZE*SIZE);
 
     double ** array = (double**) malloc(sizeof(double*) * SIZE);
@@ -63,15 +73,18 @@ int main() {
     // but then the for loop has to go since we have constants
     */
 
+    //populate array
+    double counter = 0.25;
     for ( size_t ix = 0; ix < SIZE; ++ix ) {
       for ( size_t jx = 0; jx < SIZE; ++jx ) {
         array[ix][jx] = counter;
+        counter += 0.25;
       }
     }
 
 // #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### 
 
-    printf("Starting calculation and benchmarking...\n\n");
+    printf("Starting calculation and benchmarking...\n");
 
     timespec_get(&start, TIME_UTC);
     row_sums(sumrows, array, SIZE, SIZE);
@@ -83,6 +96,19 @@ int main() {
     timespec_get(&end, TIME_UTC);
     timespec_diff(&start, &end, &diffcols);
 
-    printf("Row[0]-Sum: %f, time needed to calculate all row-sums: %lis and %lins\n", sumrows[0], diffrows.tv_sec, diffrows.tv_nsec);
-    printf("Col[0]-Sum: %f, time needed to calculate all col-sums: %lis and %lins\n", sumcols[0], diffcols.tv_sec, diffcols.tv_nsec);
+    timespec_get(&start, TIME_UTC);
+    col_sums2(sumcols2, array, SIZE, SIZE);
+    timespec_get(&end, TIME_UTC);
+    timespec_diff(&start, &end, &diffcols2);
+
+    //check for correctness of col_sums2
+    printf("Calculations are done, checking for correctness...\n");
+    for(size_t i = 0; i < SIZE; i++) {
+        assert(sumcols[i] == sumcols2[i]);
+    }
+    printf("Results are correct!\n\n");
+
+    printf("Row[0]-Sum: %f,\t time needed to calculate all row-sums: %lis and %lins\n", sumrows[0], diffrows.tv_sec, diffrows.tv_nsec);
+    printf("Col[0]-Sum: %f,\t time needed to calculate all col-sums: %lis and %lins\n", sumcols[0], diffcols.tv_sec, diffcols.tv_nsec);
+    printf("Col[0]-Sum: %f,\t time needed to calculate all col-sums: %lis and %lins <-- with improved algorithm\n", sumcols2[0], diffcols2.tv_sec, diffcols2.tv_nsec);
 }
