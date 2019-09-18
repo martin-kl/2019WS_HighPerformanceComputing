51424ab6e3d8d4d858b30cca2f6caa5d38c57239
Martin Klampfer <e1526110@student.tuwien.ac.at>
2019-09-18 15:05:49 +0200

    locality also loops and takes avergae & finished notes

--- Assignment_1/locality.c
+++ Assignment_1/locality.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
+#include <string.h>
 #include <time.h>
 #include "time_diff.h"
 
 #define SIZE 1000
+#define LOOP 100
 
 // Given implementation of row and column sumns
 void row_sums(double * sums, const double ** matrix, size_t nrs, size_t ncs) {
@@ -75,29 +76,69 @@ int main() {
 
     printf("Starting calculation and benchmarking...\n");
 
+    printf("\tStarting row sums...\n");
     timespec_get(&start, TIME_UTC);
+    for(size_t i = 0; i < LOOP-1; i++) {
+        row_sums(sumrows, array, SIZE, SIZE);
+        memset(sumrows, 0, sizeof(double) * SIZE);
+    }
     row_sums(sumrows, array, SIZE, SIZE);
     timespec_get(&end, TIME_UTC);
     timespec_diff(&start, &end, &diffrows);
 
+    printf("\tStarting col sums...\n");
     timespec_get(&start, TIME_UTC);
+    for(size_t i = 0; i < LOOP-1; i++) {
+        col_sums(sumcols, array, SIZE, SIZE);
+        memset(sumcols, 0, sizeof(double) * SIZE);
+    }
     col_sums(sumcols, array, SIZE, SIZE);
     timespec_get(&end, TIME_UTC);
     timespec_diff(&start, &end, &diffcols);
 
+    printf("\tStarting optimized col sums...\n");
     timespec_get(&start, TIME_UTC);
+    for(size_t i = 0; i < LOOP-1; i++) {
+        col_sums2(sumcols2, array, SIZE, SIZE);
+        memset(sumcols2, 0, sizeof(double) * SIZE);
+    }
     col_sums2(sumcols2, array, SIZE, SIZE);
     timespec_get(&end, TIME_UTC);
     timespec_diff(&start, &end, &diffcols2);
 
     //check for correctness of col_sums2
     printf("Calculations are done, checking for correctness...\n");
+    int correct = 1;
     for(size_t i = 0; i < SIZE; i++) {
-        assert(sumcols[i] == sumcols2[i]);
+        //assert(sumcols[i] == sumcols2[i]);
+        if(sumcols[i] != sumcols2[i]) {
+            correct = 0;
+            printf("DEVIATION! sumcols[%li] = %f, sumcols2[%li] = %f\n", i, sumcols[i], i, sumcols[2]);
+        }
     }
-    printf("Results are correct!\n\n");
+    if(!correct)
+        printf("\nATTENTION: Errors have been found!!!!!\n\n");
+    else 
+        printf("Colsums are equal!\n\n");
 
-    printf("Row[0]-Sum: %f,\t time needed to calculate all row-sums: %lis and %fms\n", sumrows[0], diffrows.tv_sec, (diffrows.tv_nsec / 1000000.0));
-    printf("Col[0]-Sum: %f,\t time needed to calculate all col-sums: %lis and %fms\n", sumcols[0], diffcols.tv_sec, (diffcols.tv_nsec / 1000000.0));
-    printf("Col[0]-Sum: %f,\t time needed to calculate all col-sums: %lis and %fms <-- with improved algorithm\n", sumcols2[0], diffcols2.tv_sec, (diffcols2.tv_nsec / 1000000.0));
+
+    //printing times
+    double avg = diffrows.tv_sec * 1000;
+    avg += (diffrows.tv_nsec / 1000000.0);
+    avg /= LOOP;
+
+    printf("Row[0]-Sum: %f,\nOverall time needed to calculate all row-sums %d times: %lis and %fms\n", sumrows[0], LOOP, diffrows.tv_sec, (diffrows.tv_nsec / 1000000.0));
+    printf("Avg time for row-sums: %fms\n\n", avg);
+
+    avg = diffcols.tv_sec * 1000;
+    avg += (diffcols.tv_nsec / 1000000.0);
+    avg /= LOOP;
+    printf("Col[0]-Sum: %f,\nOverall time needed to calculate all col-sums %d times: %lis and %fms\n", sumcols[0], LOOP, diffcols.tv_sec, (diffcols.tv_nsec / 1000000.0));
+    printf("Avg time for col-sums: %fms\n\n", avg);
+
+    avg = diffcols2.tv_sec * 1000;
+    avg += (diffcols2.tv_nsec / 1000000.0);
+    avg /= LOOP;
+    printf("Col[0]-Sum: %f,\nOverall time needed to calculate all col-sums %d times: %lis and %fms <-- with improved algorithm\n", sumcols2[0], LOOP, diffcols2.tv_sec, (diffcols2.tv_nsec / 1000000.0));
+    printf("Avg time for col2-sums: %fms\n\n", avg);
 }
