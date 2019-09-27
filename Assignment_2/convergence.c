aadee308d8c91ea6faf76d2c2cd1cb901ae3abbc
santiama01 <amayas@student.chalmers.se>
2019-09-27 21:31:21 +0200

    harcoded roots in convergence method

--- Assignment_2/convergence.c
+++ Assignment_2/convergence.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <complex.h>
 #include <math.h>
-int main() {
+#define M_PI 3.14159265358979323846
 
+int main(){
 
-	size_t poly = 3;
+	// hardcode roots
+	double complex root[9][9] = {{1,0,0,0,0,0,0,0,0}, // roots for x - 1, d = 1
+	                            {1,-1,0,0,0,0,0,0,0}, // roots for x^2 - 1, d = 2
+															{1,-0.5 + 0.5*sqrt(3)*I,-0.5 - 0.5*sqrt(3)*I,0,0,0,0,0,0}, // roots for x^3 - 1, d = 3
+															{1,-1,I,-I,0,0,0,0,0}, // roots for x^4 - 1, d = 4
+															{1,-1/4 - sqrt(5)/4 - I*sqrt(5/8 - sqrt(5)/8),-1/4 - sqrt(5)/4 + I*sqrt(5/8 - sqrt(5)/8),-1/4 + sqrt(5)/4 - I*sqrt(5/8 + sqrt(5)/8),-1/4 + sqrt(5)/4 + I*sqrt(5/8 + sqrt(5)/8),0,0,0,0}, // roots for x^5 - 1, d = 5
+															{1,-1,-0.5 - 0.5*sqrt(3)*I,0.5 + 0.5*sqrt(3)*I, 0.5 - 0.5*sqrt(3)*I,-0.5 + 0.5*sqrt(3)*I,0,0,0}, // roots for x^6 - 1, d = 6
+															{1,-cos(M_PI/7) - sin(M_PI/7)*I,sin((3*M_PI)/14) + cos((3*M_PI)/14)*I,-sin(M_PI/14) - cos(M_PI/14)*I,-sin(M_PI/14) + cos(M_PI/14)*I,sin((3*M_PI)/14) - cos((3*M_PI)/14)*I,-cos(M_PI/7) + sin(M_PI/7)*I,0,0}, // roots for x^7 - 1, d = 7
+															{1,-1,I,-I,(1 + I)/sqrt(2),(-1 - I)/sqrt(2),(1 - I)/sqrt(2),(-1 + I)/sqrt(2),0}, // roots for x^8 - 1, d = 8
+															{1,-cos(M_PI/9) - I*sin(M_PI/9),0.5*sqrt(3)*sin(M_PI/9) + 0.5*cos(M_PI/9) + I*(0.5*sqrt(3)*cos(M_PI/9) - 0.5*sin(M_PI/9)),-0.5 - 0.5*I*sqrt(3),sin(M_PI/18) + I*cos(M_PI/18),sin(M_PI/18) - I*cos(M_PI/18),-0.5* + 0.5*I*sqrt(3),0.5*sqrt(3)*sin(M_PI/9) + 0.5*cos(M_PI/9) + I*(0.5*sin(M_PI/9) - 0.5*sqrt(3)*cos(M_PI/9)),-cos(M_PI/9) + I*sin(M_PI/9)}}; // roots for x^9 - 1, d = 9
+
+	size_t d = 3;
 	double complex x0 = -3 + 1*I;
 	double complex x1;
 	double epsilon = 0.001;
-	double complex root[4] = {1,-0.5 + 0.5*sqrt(3)*I,-0.5 - 0.5*sqrt(3)*I};
 	short int root_id = 0;
 	double n_root = 100000000000;
 
@@ -16,12 +27,12 @@ int main() {
 	int iterations = 0;
 	while(conv == -1) {
 		iterations += 1;
-		x1 = x0 - (cpow(x0,poly)-1)/(poly*cpow(x0,poly-1));
+		x1 = x0 - (cpow(x0,d)-1)/(d*cpow(x0,d-1));
 	 	printf("%.15f, %.15f\n", creal(x1),cimag(x1));
 
-		for (size_t ix = 0; ix < poly + 1; ix++) {
-			if (cabs(x1 - root[ix] ) <= epsilon) {
-				printf("this point converges to root number %ld = %.15f + %.15f\n",ix,creal(root[ix]),cimag(root[ix]));
+		for (size_t ix = 0; ix < d + 1; ix++) {
+			if (cabs(x1 - root[d-1][ix] ) <= epsilon) {
+				printf("this point converges to root number %ld = %.15f + %.15f\n",ix,creal(root[d-1][ix]),cimag(root[d-1][ix]));
 				conv = 1;
 				break;
 			}
@@ -29,7 +40,7 @@ int main() {
 
 		x0 = x1;
 	}
-	printf("%d\n",iterations);
+	printf("number of iterations %d\n",iterations);
 
 		return 0;
 }
