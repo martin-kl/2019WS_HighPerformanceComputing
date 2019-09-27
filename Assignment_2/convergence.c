c982e0a3e31a20c86d4438e939b449558de6a9a2
santiama01 <amayas@student.chalmers.se>
2019-09-27 18:49:49 +0200

    conv2.0

--- Assignment_2/convergence.c
+++ Assignment_2/convergence.c
@@ -4,33 +4,32 @@
 int main() {
 
 
-	size_t d = 3;
+	size_t poly = 3;
 	double complex x0 = -3 + 1*I;
 	double complex x1;
 	double epsilon = 0.001;
-	double complex root[3] = {1,-0.5 + 0.5*sqrt(3)*I,-0.5 - 0.5*sqrt(3)*I};
+	double complex root[4] = {1,-0.5 + 0.5*sqrt(3)*I,-0.5 - 0.5*sqrt(3)*I};
 	short int root_id = 0;
+	double n_root = 100000000000;
 
-for (size_t i = 0; i < 10; i++) {
-	x1 = x0 - (cpow(x0,d)-1)/(d*cpow(x0,d-1));
- 	printf("%.15f, %.15f\n", creal(x1),cimag(x1));
-	if (cabs(x1 - root[0] ) <= epsilon) {
-		root_id = 0;
-		printf("this point converges to root number %d = %.15f + %.15f\n",root_id,creal(root[root_id]),cimag(root[root_id]));
-		break;
-	}
-	else if (cabs(x1 - root[1] ) <= epsilon) {
-		root_id = 1;
-		printf("this point converges to root number %d = %.15f + %.15f\n",root_id,creal(root[root_id]),cimag(root[root_id]));
-		break;
-	}
-	else if (cabs(x1 - root[2] ) <= epsilon) {
-		root_id = 2;
-		printf("this point converges to root number %d = %.15f + %.15f\n",root_id,creal(root[root_id]),cimag(root[root_id]));
-		break;
-	}
-	x0 = x1;
-}
+	short int conv = -1;
+	int iterations = 0;
+	while(conv == -1) {
+		iterations += 1;
+		x1 = x0 - (cpow(x0,poly)-1)/(poly*cpow(x0,poly-1));
+	 	printf("%.15f, %.15f\n", creal(x1),cimag(x1));
 
-	return 0;
+		for (size_t ix = 0; ix < poly + 1; ix++) {
+			if (cabs(x1 - root[ix] ) <= epsilon) {
+				printf("this point converges to root number %ld = %.15f + %.15f\n",ix,creal(root[ix]),cimag(root[ix]));
+				conv = 1;
+				break;
+			}
+		}
+
+		x0 = x1;
+	}
+	printf("%d\n",iterations);
+
+		return 0;
 }
