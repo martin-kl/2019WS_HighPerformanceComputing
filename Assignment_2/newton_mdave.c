d8990b3aacdf652ff9085bf855201d1fb62d7986
beingDave <dave.maitreya@gmail.com>
2019-10-01 01:28:20 +0200

    Update newton_mdave.c

--- Assignment_2/newton_mdave.c
+++ Assignment_2/newton_mdave.c
@@ -1,48 +1,71 @@
 #include <stdio.h>
 #include <complex.h>
+#include <math.h>
+#include <time.h>
 
-double x1r, x1c;
-//complex c1,c2;
-double fcn(double val, size_t pow) {
-	if(pow == 1)
-		return val;
-	else if(pow == 0)
-		return 1;
+#define NUMBER 100000
+#define M_PI 3.14159265358979323846
 
-	return val*fcn(val, pow-1);
-	}
-
-void newton(size_t c, size_t d, double x0r, double x0c) {
-	for(size_t i = 0; i < c; i++) {
-	  double 
-	  //x1r = x0r - ( (fcn(x0r, d) - 1) / (d*(fcn(x0r, (d-1)))) );
-	  //		x1c = x0c - ( (fcn(x0c, d) - 1) / (d*(fcn(x0c, (d-1)))) );
-		x0r = x1r;
-		x0c = x1c;
-		printf("%.14f\t%.14fi\n", x1r, x1c);
-	}
+void timespec_diff(struct timespec *start, struct timespec *stop, struct timespec *result)
+{
+    if ((stop->tv_nsec - start->tv_nsec) < 0)
+    {
+        result->tv_sec = stop->tv_sec - start->tv_sec - 1;
+        result->tv_nsec = stop->tv_nsec - start->tv_nsec + 1000000000;
+    }
+    else
+    {
+        result->tv_sec = stop->tv_sec - start->tv_sec;
+        result->tv_nsec = stop->tv_nsec - start->tv_nsec;
+    }
+    return;
 }
 
-  //z1r = creal(z)
-  //z1i = cimag(z);
-int main() {
-  double complex c2;
-  double complex c1;
-  double complex d = 3;
-	double xr = -3.0;
-	double xc = 1.0;
-	c1 = -3.0 + 1.0*I;
-	size_t cnt = 10;
-	//printf("Real\t\tComplex\n");
-	//printf("%.14f\t%.14f\n", xr, xr);
-	//	newton(cnt, d, xr, xc);
-
-	for(int i = 10; i > 0; i--) {
-	  c2 = c1 - ((cpow(c1, d)-1)/(d*cpow(c1, d-1)));
-	  printf("%.15f + i%.15f\n", creal(c2), cimag(c2));
-	  c1 = c2;
+static inline double complex compute(double complex z, double d, int c) {
+	double real, imag, add_for_correction, r, theta;
+	double complex res;
+	
+	for(int i = c; i > 0; i--) {
+		real = creal(z);
+		imag = cimag(z);
+		add_for_correction = 0;
+	
+		if(real < 0) add_for_correction = M_PI;
+		else if (imag < 0) add_for_correction = 2 * M_PI;
+	
+		r = sqrt(real * real + imag *imag);
+		theta = atan(imag / real) + add_for_correction;
+		
+		res = z - ( ((pow(r, d) * (cos(d * theta) + I * sin(d * theta)))-1) / (d*(pow(r, (d-1)) * (cos((d-1) * theta) + I * sin((d-1) * theta)))) );
+		z = res;
 	}
+	
+	return res;
+}
+
+
+int main() {
+    struct timespec start;
+    struct timespec end;
+    struct timespec diff, diff1;
+
+    double complex z = 1.0 + 1.0 * I;
+    double d = 5.0;
+    double complex res;
+	
+	int c = 12;
+	
+    timespec_get(&start, TIME_UTC);
+    for (size_t i = 0; i < 1; ++i) {
+        res = compute(z, d, c);
+    }
+	
+    timespec_get(&end, TIME_UTC);
+    timespec_diff(&start, &end, &diff);
+
+    printf("Result 1 :\t%.10f%+.10fi\n", creal(res), cimag(res));
+    printf("\n");
+    printf("Time 1 :\t%lis and %fms\n", diff.tv_sec, (diff.tv_nsec / 1000000.0));
 	
 	return 0;
 }
-	
