1a343bed4869c9d67ca3b0b13a8afcd8f65137ea
santiama01 <amayas@student.chalmers.se>
2019-10-12 09:36:48 +0200

    Update cell_distances.c

--- Assignment_3/cell_distances.c
+++ Assignment_3/cell_distances.c
@@ -76,8 +76,6 @@ int main(int argc, char *argv[])
 
         for (size_t ix = 0; ix < FIXED_BLOCK_SIZE; ix++) {
 
-          fixed_points[ix] = allowed_block[ix]; // initializing fixed block
-
           for (size_t kx = 1; kx < ALLOWED_BLOCK_SIZE - ix; kx++) {
 
             dist_temp = fixed_points[ix] - allowed_block[kx + ix]; // calculating distances from each element of fixed block to all the elements of the current allowed block
