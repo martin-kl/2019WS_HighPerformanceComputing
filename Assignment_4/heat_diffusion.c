6254c06523db10787ec8b67bb2d4d7b0575a34ba
Martin Klampfer <e1526110@student.tuwien.ac.at>
2019-10-18 12:03:34 +0200

    changed everything to cl_double and fixed width/height

--- Assignment_4/heat_diffusion.c
+++ Assignment_4/heat_diffusion.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
     //variables
     double avg;
     size_t width, height, nmb_points, nmb_points_padding;
-    cl_float *heat_values;
+    cl_double*heat_values;
     char *kernel_code;
 
     // OpenCL specific variables
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
     }
 
     parseArguments(argc, argv, progname, &iterations, &diffusion_const);
-    printf("Called with %d iterations and diffusion const of %f\n", iterations, diffusion_const);
+    //printf("Called with %d iterations and diffusion const of %f\n", iterations, diffusion_const);
 
     if ((fp_diffusion = fopen(DIFFUSION_FILE, "r")) == NULL)
     {
@@ -67,23 +67,27 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Cannot read width and height from diffusion file!\n");
         exit(EXIT_FAILURE);
     }
-    printf("width = %lu, height = %lu\n", width, height);
+    //printf("width = %lu, height = %lu\n", width, height);
 
     //calloc heat values
     nmb_points = width * height;
     width += 2;
     height += 2;
     nmb_points_padding = width * height;
-    heat_values = (cl_float *) calloc(nmb_points_padding, sizeof(cl_float));
 
+    heat_values = (cl_double *) malloc(nmb_points_padding* sizeof(cl_double));
+    if(heat_values == NULL) {
+        fprintf(stderr, "Error on allocating memory for the heat values\n");
+        exit(EXIT_FAILURE);
+    }
+    long unsigned row, col;
+    double value;
     while (!feof(fp_diffusion)) {
-        long unsigned x, y;
-        float value;
-        if(fscanf(fp_diffusion, "%lu %lu %f\n", &x, &y, &value) != 3) {
+        if(fscanf(fp_diffusion, "%lu %lu %lf\n", &col, &row, &value) != 3) {
             fprintf(stderr, "Error while parsing input values from diffusion file!\n");
             exit(EXIT_FAILURE);
         }
-        heat_values[(x+1) * width + (y+1)] = value;
+        heat_values[(row+1) * width + col+1] = value;
     }
     fclose(fp_diffusion);
 
@@ -105,6 +109,7 @@ int main(int argc, char *argv[])
     //go back to beginning of file
     fseek(fp_kernel, 0, SEEK_SET);
     //allocate memory
+
     kernel_code = (char *) malloc(src_size * sizeof(char));
     if (kernel_code == NULL) {
         fprintf(stderr, "Could not allocate memory for kernel!\n");
@@ -113,7 +118,6 @@ int main(int argc, char *argv[])
     src_size = fread(kernel_code, sizeof(char), src_size, fp_kernel);
     fclose(fp_kernel);
 
-
     //get platform information
     if (clGetPlatformIDs(1, &platform_id, &nmb_platforms) != CL_SUCCESS) {
         fprintf(stderr, "cannot get platform\n");
@@ -121,15 +125,13 @@ int main(int argc, char *argv[])
     }
 
     //get device information
-    if (clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU,
-          1, &device_id, &nmb_devices) != CL_SUCCESS) {
+    if (clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, &nmb_devices) != CL_SUCCESS) {
         fprintf(stderr, "cannot get device\n");
         exit(EXIT_FAILURE);
     }
 
-    //create an opencl context
-    cl_context_properties properties[] = { CL_CONTEXT_PLATFORM,
-          (cl_context_properties) platform_id, 0 };
+    //create opencl context
+    cl_context_properties properties[] = { CL_CONTEXT_PLATFORM, (cl_context_properties) platform_id, 0 };
     context = clCreateContext(properties, 1, &device_id, NULL, NULL, &error);
     if (error != CL_SUCCESS) {
         fprintf(stderr, "cannot create context\n");
@@ -143,36 +145,33 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    // create a program from kernel source code
-    program = clCreateProgramWithSource(context, 1, 
-          (const char **) &kernel_code, 
+    //create program from kernel source
+    program = clCreateProgramWithSource(context, 1, (const char **) &kernel_code, 
           (const size_t *) &src_size, &error);
     if (error != CL_SUCCESS) {
         fprintf(stderr, "cannot create program object\n");
         exit(EXIT_FAILURE);
     }
 
-    // build the program
-    // error = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+    //build the program
+    //error = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
     error = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
     if (error != CL_SUCCESS) {
         fprintf(stderr, "Cannot build program. log:\n");
         size_t log_size = 0;
-        clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG,
-              0, NULL, &log_size);
+        clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
         char *log = calloc(log_size, sizeof(char));
         if (log == NULL) {
             fprintf(stderr, "Could not allocate memory for log!\n");
             exit(EXIT_FAILURE);
         }
-        clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG,
-            log_size, log, NULL);
+        clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
         fprintf(stderr, "%s\n", log);
         free(log);
         exit(EXIT_FAILURE);
     }
 
-    // create opencl kernel
+    //create opencl kernel
     cl_kernel kernel;
     kernel = clCreateKernel(program, "diffusion", &error);
     if (error != CL_SUCCESS) {
@@ -180,23 +179,23 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    // create memory buffers on the device (i.e. GPU)
+    //create memory buffers on the device (i.e. GPU)
     cl_mem buffer_boxes = clCreateBuffer(context, CL_MEM_READ_WRITE,
-          sizeof(float) * nmb_points_padding, NULL, &error);
+          sizeof(cl_double) * nmb_points_padding, NULL, &error);
     if (error != CL_SUCCESS) {
         fprintf(stderr, "cannot create input memory buffer for boxes\n");
         exit(EXIT_FAILURE);
     }
 
-    // copy to the respective memory buffers
+    //copy to the respective memory buffers
     error = clEnqueueWriteBuffer(command_queue, buffer_boxes, CL_TRUE, 0,
-          sizeof(float) * nmb_points_padding, heat_values, 0, NULL, NULL);
+          sizeof(cl_double) * nmb_points_padding, heat_values, 0, NULL, NULL);
     if (error != CL_SUCCESS) {
         fprintf(stderr, "cannot write boxes to input buffer\n");
         exit(EXIT_FAILURE);
     }
 
-    // set arguments of kernel
+    //set arguments of kernel
     error = clSetKernelArg(kernel, 0, sizeof(cl_mem), &buffer_boxes);
     if (error != CL_SUCCESS) {
         fprintf(stderr, "cannot set kernel argument for buffer_boxes\n");
@@ -208,7 +207,7 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    // execute opencl kernel
+    //execute opencl kernel
     const size_t global[] = {width, height};
     for (size_t i = 0; i < iterations; ++i) {
         error = clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL,
@@ -217,36 +216,38 @@ int main(int argc, char *argv[])
             fprintf(stderr, "cannot execute kernel\n");
             exit(EXIT_FAILURE);
         }
-        // check status when debugging
-        // status = clFlush(command_queue);
-        // status = clFinish(command_queue);
+        //check status when debugging
+        //status = clFlush(command_queue);
+        //status = clFinish(command_queue);
     }
 
-    // read buffer on device memory to host memory
+    //read buffer on device memory to host memory
     error = clEnqueueReadBuffer(command_queue, buffer_boxes, CL_TRUE, 0,
-          sizeof(float) * nmb_points_padding, heat_values, 0, NULL, NULL);
+          sizeof(cl_double) * nmb_points_padding, heat_values, 0, NULL, NULL);
     if (error != CL_SUCCESS) {
         fprintf(stderr, "cannot read from buffer\n");
         exit(EXIT_FAILURE);
     }
-    // print out the result boxes
-    // for (int h = 0; h < height; ++h) {
-    //     for (int w = 0; w < width; ++w) {
-    //         printf("%g ", heat_values[h*width+w]);
-    //     }
-    //     printf("\n");
-    // }
+    //print out the result boxes
+    //for (int h = 0; h < height; ++h) {
+    //   for (int w = 0; w < width; ++w) {
+    //        printf("%g ", heat_values[h*width+w]);
+    //    }
+    //    printf("\n");
+    //}
 
-    // block until all previously queued opencl commands have completed
+    //block until all previously queued opencl commands have completed
     status = clFinish(command_queue);
 
+    /*
     printf("final values:\n");
-    for (size_t i = 1; i < width-1; i++) {
-        for (size_t j = 1; j < height-1; j++) {
+    for (size_t i = 1; i < height-1; i++) {
+        for (size_t j = 1; j < width-1; j++) {
             printf("%f ", heat_values[i * width + j]);
         }
         printf("\n");
     }
+    */
 
     printf("\n\n");
     avg = 0;
@@ -256,9 +257,9 @@ int main(int argc, char *argv[])
     // print average temperature
     printf("average: %g\n", avg);
     
-    float diff;
-    for (size_t i = 1; i < width-1; i++) {
-        for (size_t j = 1; j < height-1; j++) {
+    double diff;
+    for (size_t i = 1; i < height-1; i++) {
+        for (size_t j = 1; j < width-1; j++) {
             diff = heat_values[i * width + j] - avg;
             printf("%f ",((diff < 0) ? (-diff) : diff));
         }
